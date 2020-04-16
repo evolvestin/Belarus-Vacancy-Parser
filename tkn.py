1bada8e2692303be4f436bea463be9bd2b67b02f
steve10live <38049797+steve10live@users.noreply.github.com>
2020-04-16 20:59:27 +0300

    Update tkn.py

--- tkn.py
+++ tkn.py
@@ -1,4 +1,6 @@
-tkn = '993071212:AAFbZvEx8IJaL1_8fWNDs4qdAJHNMKTnS7U'
-idMain = -1001404073893
-idJobi = -1001272631426
-floater = 1
+idMe = 396978030
+tkn = '429683355:AAF3GReDyewByK-WRLQ44xpCNKIsYg1G8X0'  # 993071212:AAFbZvEx8IJaL1_8fWNDs4qdAJHNMKTnS7U
+idMain = idMe  # -1001404073893
+idJobi = idMe  # -1001272631426
+start_link = 9  # 8
+floater = 0  #1
